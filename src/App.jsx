6f401e2c5deec530233d74cbf8af3e52f93abe89
ASAM PRABHAKAR REDDY <asamprabhakarbala@gmail.com>
2024-08-27 11:58:24 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+        <h1>hello from vite app</h1>
+        <p>now i deployed on aws ec2</p>
+    </>
+  )
+}
+
+export default App
